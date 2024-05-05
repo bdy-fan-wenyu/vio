4e8d444ae3fb329b26d71b7b613136e1306b539b
fwy <2643660853@qq.com>
2024-05-05 09:16:56 +0800

    添加camera类

--- front_end/camera/include/camera.h
+++ front_end/camera/include/camera.h
@@ -1,24 +1,52 @@
 /**
  * @file camera.h
  * @author 范文宇 (2643660853@qq.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-04
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
-
+#include <memory>
+#include <vector>
 #include <Eigen/Core>
 #include <Eigen/Dense>
-class camera
+#include <opencv2/opencv.hpp>
+class Camera
 {
 
+public:
+    Camera()
+    {
+        images_.reserve(1000);
+        camera_out_para_.reserve(1000);
+        camera_rt_.reserve(1000);
+    };
+    ~Camera() = default;
+    // 设置相机图像
+    void setImage(cv::Mat);
+    // 获取相机图像
+    std::vector<cv::Mat> getImage();
 
+    // 设置相机内参
+    void setCameraInPara(Eigen::Matrix3f);
+    // 获取相机内参
+    Eigen::Matrix3f getCameraInPara();
 
+    // 设置相机和特征点(世界)的外参
+    void setCameraOutPara(Eigen::Matrix4f);
+    // 获取相机和特征点(世界)的外参
+    std::vector<Eigen::Matrix4f> getCameraOutPara();
 
 private:
-Eigen::Matrix3f camera_in_para_;
-Eigen::Matrix4f camera_out_para_;
-
+    // 图像
+    std::vector<cv::Mat> images_;
+    // 相机内参
+    Eigen::Matrix3f camera_in_para_;
+    // 相机外参
+    std::vector<Eigen::Matrix4f> camera_out_para_;
+    // 相机自身位姿
+    std::vector<Eigen::Matrix4f> camera_rt_;
 };
+inline std::shared_ptr<Camera> Camera_ptr;
--- front_end/camera/src/camera.cpp
+++ front_end/camera/src/camera.cpp
@@ -1,13 +1,49 @@
 /**
  * @file camera.cpp
  * @author 范文宇 (2643660853@qq.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-04
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
-
 #include <camera.h>
+using namespace cv;
+using namespace std;
+using namespace Eigen;
+
+void Camera::setImage(cv::Mat image)
+{
+    if (images_.size() > 1000)
+        images_.pop_back();
+    images_.push_back(image);
+}
+
+vector<Mat> Camera::getImage()
+{
+    return images_;
+}
+
+void Camera::setCameraInPara(Matrix3f camera_in_para)
+{
+    camera_in_para_ = camera_in_para;
+}
+
+Matrix3f Camera::getCameraInPara()
+{
+    return camera_in_para_;
+}
+
+void Camera::setCameraOutPara(Matrix4f camera_out_para)
+{
+    if (camera_out_para_.size() > 1000)
+        camera_out_para_.pop_back();
+    camera_out_para_.push_back(camera_out_para);
+}
+
+vector<Matrix4f> Camera::getCameraOutPara()
+{
+    return camera_out_para_;
+}
