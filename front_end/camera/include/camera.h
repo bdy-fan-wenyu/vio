8015e0db273153e7e2a8f1706e3d632ea83ef038
fwy <2643660853@qq.com>
2024-09-19 22:56:55 +0800

    更新camera类

--- front_end/camera/include/camera.h
+++ front_end/camera/include/camera.h
@@ -13,40 +13,73 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
+/**
+ * @brief 小孔成像相机模型（不考虑畸变），主要实现
+ *        ①世界坐标系到像素坐标系的转换
+ *        ②世界坐标系到归一化坐标系的转换
+ *        ③计算相机的外参矩阵
+ */
 class Camera
 {
 
 public:
-    Camera()
-    {
-        images_.reserve(1000);
-        camera_out_para_.reserve(1000);
-        camera_rt_.reserve(1000);
-    };
+    /**
+     * @brief 构造函数
+     *
+     * @param camera_name 相机名字
+     * @param image_width 图像宽度
+     * @param image_height 图像高度
+     * @param fx 焦点x
+     * @param fy 焦点y
+     * @param cx 像元大小x
+     * @param cy 像元大小y
+     */
+    Camera(const string &camera_name, double image_width, double image_height,
+           double fx, double fy, double cx, double cy)
+        : image_width_(image_width), image_height_(image_height),
+          fx_(fx), fy_(fy), cx_(cx), cy_(cy) {
+
+          };
+    /**
+     * @brief 析构函数
+     *
+     */
     ~Camera() = default;
-    // 设置相机图像
-    void setImage(cv::Mat);
-    // 获取相机图像
-    std::vector<cv::Mat> getImage();
+    /**
+     * @brief 根据RT和路标点的世界坐标，在相机模型中计算出路标点的像素坐标
+     *
+     * @param q 世界坐标系到相机坐标的坐标变换，四元数
+     * @param t 世界坐标系到相机坐标的坐标变换，位移量
+     * @param P_w 路标点的世界坐标
+     * @param p_uv 路标点的像素坐标
+     */
+    void spaceToPlane(const Quaternionf &q, const Eigen::Matrix<double, 3, 1> &t,
+                      const Eigen::Matrix<double, 3, 1> &P_w, Eigen::Matrix<double, 2, 1> &p_uv);
 
-    // 设置相机内参
-    void setCameraInPara(Eigen::Matrix3f);
-    // 获取相机内参
-    Eigen::Matrix3f getCameraInPara();
-
-    // 设置相机和特征点(世界)的外参
-    void setCameraOutPara(Eigen::Matrix4f);
-    // 获取相机和特征点(世界)的外参
-    std::vector<Eigen::Matrix4f> getCameraOutPara();
+    /**
+     * @brief 像素坐标系到相机归一化坐标系的转化
+     *
+     * @param p_uv 路标点的像素坐标
+     * @param p_n 路标点的归一化坐标
+     */
+    void liftProjective(const Eigen::Matrix<double, 2, 1> &p_uv, Eigen::Matrix<double, 2, 1> &p_n);
+    /**
+     * @brief 根据路标点的
+     * 
+     * @param objectpoints 
+     * @param imagepoints 
+     * @param tec 
+     * @param rec 
+     */
+    void estimateExtrinsics(const std::vector<cv::Point3f> &objectpoints, const std::vector<cv::Point2f> &imagepoints,
+                            cv::Mat tec, cv::Mat rec);
 
 private:
-    // 图像
-    std::vector<cv::Mat> images_;
-    // 相机内参
-    Eigen::Matrix3f camera_in_para_;
-    // 相机外参
-    std::vector<Eigen::Matrix4f> camera_out_para_;
-    // 相机自身位姿
-    std::vector<Eigen::Matrix4f> camera_rt_;
+    double image_width_;
+    double image_height_;
+    double fx_;
+    double fy_;
+    double cx_;
+    double cy_;
 };
 typedef std::shared_ptr<Camera> camera_ptr;
--- front_end/camera/src/camera.cpp
+++ front_end/camera/src/camera.cpp
@@ -10,40 +10,64 @@
  */
 
 #include <camera.h>
+#include "ceres/rotation.h"
 using namespace cv;
 using namespace std;
 using namespace Eigen;
 
-void Camera::setImage(cv::Mat image)
+void Camera::spaceToPlane(const Quaternionf &q, const Matrix<double, 3, 1> &t,
+                          const Matrix<double, 3, 1> &P_w, Matrix<double, 2, 1> &p_uv)
 {
-    if (images_.size() > 1000)
-        images_.pop_back();
-    images_.push_back(image);
+    // 从世界坐标系恢复到相机坐标系
+    Matrix3d rotate_matrix_cw = q.matrix();
+    Matrix<double, 3, 1> P_c = rotate_matrix_cw * P_w;
+
+    // 相机坐标系转换到归一化坐标系
+    double P_nx = P_c(0) / P_c(2);
+    double P_ny = P_c(1) / P_c(2);
+
+    // 相机坐标系到图像坐标系
+    double x = fx_ * P_nx;
+    double y = fy_ * P_ny;
+
+    // 图像坐标系到像素坐标系
+    double u = x / cx_ + image_width_ / 2;
+    double v = y / cy_ + image_height_ / 2;
+
+    p_uv(0) = u;
+    p_uv(1) = v;
 }
 
-vector<Mat> Camera::getImage()
+void Camera::liftProjective(const Eigen::Matrix<double, 2, 1> &p_uv, Eigen::Matrix<double, 2, 1> &p_n)
 {
-    return images_;
+    // 相机内参矩阵
+    Matrix<double, 3, 3> T_in;
+
+    T_in << fx_ / cx_, 0, image_width_ / 2,
+        0, fy_ / cy_, image_height_ / 2,
+        0, 0, 1;
+
+    // 像素坐标齐次化
+    Matrix<double, 3, 1> p_uv_temp;
+    p_uv_temp(0) = p_uv(0);
+    p_uv_temp(1) = p_uv(1);
+    p_uv_temp(2) = 1;
+
+    // 计算得到相机归一化坐标
+    p_n = T_in.inverse() * p_uv_temp;
 }
 
-void Camera::setCameraInPara(Matrix3f camera_in_para)
+void Camera::estimateExtrinsics(const vector<Point3f> &objectpoints, const vector<Point2f> &imagepoints,
+                                Mat tvec, Mat rvec)
 {
-    camera_in_para_ = camera_in_para;
-}
-
-Matrix3f Camera::getCameraInPara()
-{
-    return camera_in_para_;
-}
-
-void Camera::setCameraOutPara(Matrix4f camera_out_para)
-{
-    if (camera_out_para_.size() > 1000)
-        camera_out_para_.pop_back();
-    camera_out_para_.push_back(camera_out_para);
-}
-
-vector<Matrix4f> Camera::getCameraOutPara()
-{
-    return camera_out_para_;
+    vector<Point2f> normpoints;
+    for (auto imagepoint : imagepoints)
+    {
+        Vector2d p_n;
+        liftProjective(Vector2d(imagepoint.x, imagepoint.y), p_n);
+        Point2f normpoint = Point2f(p_n(0), p_n(1));
+        normpoints.push_back(normpoint);
+    }
+    // 注意这里的外参矩阵和畸变矩阵都为单位矩阵，因为世界坐标系到归一化坐标系之间并没有经过相机
+    solvePnP(objectpoints, imagepoints, cv::Mat::eye(3, 3, CV_64F), cv::noArray(), rvec, tvec);
 }
