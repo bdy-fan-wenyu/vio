e13175046d2d275672d962b431d7046506b641ba
fwy <2643660853@qq.com>
2024-09-20 21:21:49 +0800

    修改feature类别 <add> 增加三角测量法的实现

--- front_end/feature_manager/feature/include/feature.h
+++ front_end/feature_manager/feature/include/feature.h
@@ -1,12 +1,12 @@
 /**
  * @file feature.h
  * @author 范文宇 (2643660853@qq.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-05
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #pragma once
@@ -16,14 +16,72 @@
 #include <Eigen/Dense>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
-
-
-class Feature
+/**
+ * @brief 一个特征点，特征点的最小单位
+ *
+ */
+class FeaturePerFrame
 {
+public:
+    FeaturePerFrame(const Eigen::Matrix<double, 7, 1> &_point, double td)
+    {
+        cur_td = td;
+        point.x() = _point(0);
+        point.y() = _point(1);
+        point.z() = _point(2);
+        uv.x() = _point(3);
+        uv.y() = _point(4);
+        velocity.y() = _point(5);
+        velocity.y() = _point(6);
+    }
 
 
-
-
+    double cur_td;
+    Eigen::Vector3d point;    // 特征点的像素坐标（去相机畸变后的），第三维实际无效
+    Eigen::Vector2d uv;       // 特征点的原始像素坐标（未去畸变）
+    Eigen::Vector2d velocity; // 像素速度
 };
 
+/**
+ * @brief 为一个特征点可能在多个帧（frame）中被追踪到，并且在不同帧中的像素坐标都不一样，
+ *        这个结构体就是锚定FeatureID，保存特征点在所有帧中的观测信息。
+ *
+ */
+class FeaturePerId
+{
+public:
+    FeaturePerId(int _featureId, int _startFrame)
+        : feature_id(_featureId), start_frame(_startFrame), used_num(0), estimated_depth(-1.0), solve_flag(0) {}
 
+    // 当前，观测到这个特征点的最后一帧的id
+    int endFrame() { return start_frame + feature_per_frame.size() - 1; }
+
+public:
+    std::vector<FeaturePerFrame> feature_per_frame; /*特征点在不同帧中的观测描述*/
+    const int feature_id;
+    int start_frame; /*注意这个frame索引是动态调整的，总是与滑窗帧的序号保持一致*/
+    int used_num;
+    double estimated_depth;
+    int solve_flag; /* 0 haven't solve yet; 1 solve succ; 2 solve fail */
+};
+
+class FeatureManager
+{
+public:
+    FeatureManager();
+    void clearState();
+    /**
+     * @brief 三角化，求解特征点的深度以及3D坐标
+     * 
+     * @param frameCnt 当前特征点属于的帧数
+     * @param Pwb imu相对于世界坐标的位置
+     * @param Rwb imu相对于世界坐标的姿态
+     * @param tbc 相机相对于imu的平移
+     * @param rbc 相机相对于imu的旋转
+     */
+    void triangulate(int frameCnt, Eigen::Vector3d Pwb[], Eigen::Matrix3d Rwb[],
+                     Eigen::Vector3d tbc[], Eigen::Matrix3d rbc[], Eigen::Vector3d &point_3d);
+
+private:
+    std::list<FeaturePerId> features_;
+};
--- front_end/feature_manager/feature/src/feature.cpp
+++ front_end/feature_manager/feature/src/feature.cpp
@@ -1,12 +1,53 @@
 /**
  * @file feature.cpp
  * @author 范文宇 (2643660853@qq.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-05
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "feature.h"
+using namespace Eigen;
+using namespace cv;
+using namespace std;
+void FeatureManager::triangulate(int frameCnt, Vector3d Pwb[], Matrix3d Rwb[], Vector3d tbc[], Matrix3d rbc[], Vector3d &point_3d)
+{
+    for (auto feature : features_)
+    {
+        frameCnt = feature.start_frame;
+        // 计算当前帧相机相对于世界坐标系的Rwc Pwc
+        Matrix<double, 4, 3> cur_camera_pose;
+        Vector3d Rwc_cur = Rwb[frameCnt] * rbc[frameCnt];
+        Vector3d Pwc_cur = Pwb[frameCnt] + Rwb[frameCnt] * rbc[frameCnt];
+        cur_camera_pose.leftCols<3>() = Rwc_cur.transpose();
+        cur_camera_pose.leftCols<1>() = -Rwc_cur.transpose() * Pwc_cur;
+
+        // 计算下一帧相机相对于世界坐标的Rwc Pwc
+        Matrix<double, 4, 3> next_camera_pose;
+        Vector3d Rwc_next = Rwb[frameCnt + 1] * rbc[frameCnt + 1];
+        Vector3d Pwc_next = Pwb[frameCnt + 1] + Rwb[frameCnt + 1] * rbc[frameCnt + 1];
+        next_camera_pose.leftCols<3>() = Rwc_next.transpose();
+        next_camera_pose.leftCols<1>() = -Rwc_next.transpose() * Pwc_next;
+
+        // 特征点当前的像素坐标
+        Vector2d point_uv_cur = feature.feature_per_frame[0].point;
+        // 特征点下一帧的像素坐标
+        Vector2d point_uv_next = feature.feature_per_frame[1].point;
+
+        // 三角测量法
+        Matrix<double, 4, 4> design_matrix = Matrix4d::Zero();
+        design_matrix.row(0) = point_uv_cur[0] * cur_camera_pose.row(2) - cur_camera_pose.row(0);
+        design_matrix.row(1) = point_uv_cur[1] * cur_camera_pose.row(2) - cur_camera_pose.row(1);
+        design_matrix.row(2) = point_uv_next[0] * next_camera_pose.row(2) - next_camera_pose.row(0);
+        design_matrix.row(3) = point_uv_next[1] * next_camera_pose.row(2) - next_camera_pose.row(1);
+
+        Eigen::Vector4d triangulated_point;
+        triangulated_point = design_matrix.jacobiSvd(Eigen::ComputeFullV).matrixV().rightCols<1>();
+        point_3d(0) = triangulated_point(0) / triangulated_point(3);
+        point_3d(1) = triangulated_point(1) / triangulated_point(3);
+        point_3d(2) = triangulated_point(2) / triangulated_point(3); /*通过三角化，得到了特征的3D坐标*/
+    }
+}
